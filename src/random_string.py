2ff25a5bdc50f13c2c178db3826315320fba376e
kapahala0078 <takuto0078@hotmail.co.jp>
2018-05-20 14:20:59 +0900

    アルファベット、数字、混合の生成

--- src/random_string.py
+++ src/random_string.py
@@ -1,7 +1,9 @@
 import string
 import random
 
-letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 print(''.join(random.choices(string.ascii_letters, k=8)))
 #  ['R', 'n', 'J', 'T', 'k']
-# 
+
+print(''.join(random.choices(string.ascii_letters + string.digits, k=8)))
+
+print(''.join(random.choices(string.digits, k=4)))
