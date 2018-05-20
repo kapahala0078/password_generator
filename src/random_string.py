3bac9b5ff620fe9551ff9dbd69aed2bf547b9d54
kapahala0078 <takuto0078@hotmail.co.jp>
2018-05-20 15:04:09 +0900

    アルファベットの大文字小文字選択可能に

--- src/random_string.py
+++ src/random_string.py
@@ -1,9 +1,29 @@
 import string
 import random
 
-print(''.join(random.choices(string.ascii_letters, k=8)))
-#  ['R', 'n', 'J', 'T', 'k']
+# 実行する際のモードを入力
+# alphabet or digits or both
+mode = "both"
+ul = "both"
 
-print(''.join(random.choices(string.ascii_letters + string.digits, k=8)))
+for i in range(100):
+    if mode is "alphabet":
+        if ul is "upper":
+            print(''.join(random.choices(string.ascii_letters, k=8)).upper())
+        elif ul is "lower":
+            print(''.join(random.choices(string.ascii_letters, k=8)).lower())
+        elif ul is "both":
+            print(''.join(random.choices(string.ascii_letters, k=8)))
+    elif mode is "digits":
+        print(''.join(random.choices(string.digits, k=4)))
+    elif mode is "both":
+        if ul is "upper":
+            print(''.join(random.choices(string.ascii_letters + string.digits, k=8)).upper())
+        elif ul is "lower":
+            print(''.join(random.choices(string.ascii_letters + string.digits, k=8)).lower())
+        elif ul is "both":
+            print(''.join(random.choices(string.ascii_letters + string.digits, k=8)))      
+    else:
+        print("error")
 
-print(''.join(random.choices(string.digits, k=4)))
+
